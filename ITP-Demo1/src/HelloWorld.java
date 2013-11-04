ff1ce65c891b95191563290900f1aa868f4a32bf
user <user@ubuntu>
2013-11-04 06:51:10 -0800

    For-Schleife eingebaut.

--- ITP-Demo1/src/HelloWorld.java
+++ ITP-Demo1/src/HelloWorld.java
@@ -1,11 +1,11 @@
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello World!");
-		System.out.println("Second message!");
 
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello World!");
+			System.out.println("Second message!");
+		}
 	}
 
 }
