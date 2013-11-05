01da357efee5c799ebe88c193e81e2104576f18c
user <user@ubuntu>
2013-11-05 08:13:29 -0800

    Now using helper method.

--- ITP-Demo1/src/HelloWorld.java
+++ ITP-Demo1/src/HelloWorld.java
@@ -3,12 +3,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
-		System.out.println("Second message!");
+		System.out.println("This is my message: " + helper());
 
 	}
 	
-	protected String helper() {
+	protected static String helper() {
 		return "Hello everybody!";
 	}
 
