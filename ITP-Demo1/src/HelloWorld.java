050c4fc26bf6a7bbe08b5bde73e9c090fe1e6fef
user <user@ubuntu>
2013-11-06 05:04:10 -0800

    Changed the messages again.

--- ITP-Demo1/src/HelloWorld.java
+++ ITP-Demo1/src/HelloWorld.java
@@ -2,13 +2,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 10; i++) {
-			System.out.println("This is my message: " + helper());
-		}
+		System.out.println("Now an even shorter message: " + helper());
+
 	}
 
 	protected static String helper() {
-		return "Hello everybody, I am glad that I finally found a way...";
+		return "Do you like this?";
 	}
 
 }
